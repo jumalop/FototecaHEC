b1d35884cf21d964e29b65cbbbec28b30b3dd66e
jumalop <114548222+jumalop@users.noreply.github.com>
2025-05-09 22:02:07 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-const CACHE_NAME = 'fototeca-v2';
+const CACHE_NAME = 'fototeca-v3';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/icon-192.png',
-    '/icon-512.png'
+  '/FototecaHEC/',
+  '/FototecaHEC/index.html',
+  '/FototecaHEC/styles.css',
+  '/FototecaHEC/app.js',
+  '/FototecaHEC/icon-192.png',
+  '/FototecaHEC/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ASSETS))
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+});
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-);
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+});
